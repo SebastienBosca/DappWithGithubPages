881266cfd14eaffc7a24dddaeff88bbe5147b01e
SebastienBosca <sbosca@wanadoo.fr>
2022-03-03 20:54:56 +0100

    Updates

---
+++ precache-manifest.b4299f72cfffe1ef323c62e0a18923a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f76970af35fe28cfee7023e124e4d45",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed0b4536932936a12fcf",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "ea0fbaa99c76ee4f0996",
+    "url": "/static/js/2.3e98137a.chunk.js"
+  },
+  {
+    "revision": "ed0b4536932936a12fcf",
+    "url": "/static/js/main.e4b4fc8a.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  }
+]);
